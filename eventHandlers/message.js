45d65aa854da5b5f46bdb4a1d03ef38b76459d6a
HologramSteve <exlkip@gmail.com>
2026-05-21 14:59:52 +0200

    feat: display tokens/s under message

--- eventHandlers/message.js
+++ eventHandlers/message.js
@@ -10,7 +10,7 @@ async function handleMessage(message, ai) {
 
         const reply = await ai.chat("(this message has been sent by user " + message.author.tag + ", with user id " + message.author.id + " and display name " + message.author.displayName + "): " + message.content);
 
-        await message.reply(reply.content + "\n-# Tokens (in/out): " + reply.usage.tokens_in + "/" + reply.usage.tokens_out);
+        await message.reply(reply.content + "\n-# Tokens (in/out): " + reply.usage.tokens_in + "/" + reply.usage.tokens_out + "\n-# Speed: " + reply.speed);
     } catch (error) {
         console.error("Error AI response:", error);
     }
