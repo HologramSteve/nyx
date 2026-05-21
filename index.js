eae57dd9e72a6f6d0c5e165bd9eb96a162d2679b
HologramSteve <exlkip@gmail.com>
2026-05-21 14:50:31 +0200

    fix: use clientReady instead of ready event following warning.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { discordClient, aiClient, discordToken } from "./construct/client.js";
 import { handleMessage } from "./eventHandlers/message.js";
 import { handleReady } from "./eventHandlers/ready.js";
 
-discordClient.once("ready", handleReady);
+discordClient.once("clientReady", handleReady);
 discordClient.on("messageCreate", (msg) => handleMessage(msg, aiClient));
 
 
